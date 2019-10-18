8e328596e4ecddfe6d363a5fda0093e74902eeda
Amanda Murphy <amurphy@newrelic.com>
2019-10-18 15:41:59 -0700

    remove ports

--- 9.2.0/docker-entrypoint.sh
+++ 9.2.0/docker-entrypoint.sh
@@ -1,18 +1,13 @@
-#!/bin/bash
-set -eo pipefail
+#!/bin/sh
+set -e
 
-runDaemon="/usr/bin/newrelic-daemon --watchdog-foreground --logfile /proc/self/fd/1"
-addressRegex="^\s*address\s*="
 NEWRELIC_CONFIG_PATH="$(echo -e "${NEWRELIC_CONFIG_PATH}" | tr -d '[:space:]')"
 configPath="${NEWRELIC_CONFIG_PATH:-/etc/newrelic/newrelic.cfg}"
 
 if [ -f $configPath ]; then
-  runDaemon="$runDaemon --c $configPath"
-  if grep -q "$addressRegex" $configPath && [ -z "${NEWRELIC_DAEMON_PORT}" ]; then
-    eval $runDaemon
-    exit
-  fi
+  set -- "$@" --c $configPath
 fi
 
-runDaemon="$runDaemon --address=$(hostname):${NEWRELIC_DAEMON_PORT:-31339}"
-eval $runDaemon
+set -- "$@"  --address=$(hostname):31339
+
+exec /usr/bin/newrelic-daemon --watchdog-foreground "$@"
