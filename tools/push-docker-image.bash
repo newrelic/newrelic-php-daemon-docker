e2085e58171bca8d571421e45bbe0ff604a5431b
Amanda Murphy <amurphy@newrelic.com>
2019-10-31 10:10:18 -0700

    change to php

--- tools/push-docker-image.bash
+++ tools/push-docker-image.bash
@@ -4,8 +4,8 @@ version=$1
 
 cd ${version} || exit 1
 
-docker build -t newrelic/c-daemon:latest -t newrelic/c-daemon:${version} .
+docker build -t newrelic/php-daemon:latest -t newrelic/php-daemon:${version} .
 
 docker login --username=newrelicphp --password ${DOCKER_HUB_PASSWORD}
 
-docker push newrelic/c-daemon
+docker push newrelic/php-daemon
