b4db8c556253bc33c65503668dda786bd5205939
Amanda Murphy <amurphy@newrelic.com>
2019-10-31 10:56:06 -0700

    PR comments

--- tests/image-tests.sh
+++ tests/image-tests.sh
@@ -8,13 +8,19 @@ cleanup()
 
 verify_output()
 {
-  if [ $1 ]; then
-    echo "FAILED: " $3
-    cleanup $2
-    exit 1
+  name=$1
+  msg=$2
+
+  shift
+  shift
+
+  if [ "$@" ]; then
+    echo "PASSED: " $msg
+    cleanup $name
   else
-    echo "PASSED: " $3
-    cleanup $2
+    echo "FAILED: " $msg
+    cleanup $name
+    exit 1
   fi
 }
 
@@ -23,49 +29,36 @@ run_tests() {
 
   name=version-check
   docker run -d --name $name c-daemon
-  verify_output "-n $(docker logs $name | grep ${1//\/})" $name "check the version"
+  verify_output $name "check the version" -n "$(docker logs $name | grep ${1//\/})"
 
   name=debug-daemon
   docker run -d --name $name c-daemon --loglevel debug
-  verify_output "-n $(docker logs $name | grep Debug)" $name "set the loglevel to debug"
+  verify_output $name "set the loglevel to debug" -n "$(docker logs $name | grep Debug)"
 
   name=debug-daemon-with-cmd
   docker run -d --name $name c-daemon --loglevel debug
-  verify_output "-n $(docker logs $name | grep Debug)" $name "set the loglevel to debug using cmd override"
+  verify_output $name "set the loglevel to debug using cmd override" -n "$(docker logs $name | grep Debug)"
 
   name=no-logs
   docker run -d --name $name c-daemon --logfile /var/log/newrelic/newrelic.log
-  verify_output "-n $(docker exec $name sh -c 'exec cat /var/log/newrelic/newrelic.log')" $name "set the logfile to /var/log/newrelic/newrelic.log"
+  verify_output $name "set the logfile to /var/log/newrelic/newrelic.log" -n "$(docker exec $name sh -c 'exec cat /var/log/newrelic/newrelic.log')"
 
   name=no-logs-with-cmd
   docker run -d --name $name c-daemon /usr/bin/newrelic-daemon --logfile /var/log/newrelic/newrelic.log
-  verify_output "-n $(docker exec $name sh -c 'exec cat /var/log/newrelic/newrelic.log')" $name "set the logfile to /var/log/newrelic/newrelic.log using cmd override"
+  verify_output $name "set the logfile to /var/log/newrelic/newrelic.log using cmd override" -n "$(docker exec $name sh -c 'exec cat /var/log/newrelic/newrelic.log')"
 
   name=cfg-debug
   echo "loglevel=debug" > newrelic.cfg
   docker run -d --name $name -v $PWD/newrelic.cfg:/etc/newrelic/newrelic.cfg c-daemon --c /etc/newrelic/newrelic.cfg
-  verify_output "-n $(docker logs $name | grep Debug)" $name "set the loglevel to debug using cfg file"
+  verify_output $name "set the loglevel to debug using cfg file" -n "$(docker logs $name | grep Debug)"
 
 }
 
-versions=(*/)
-
-# directories that don't have a docker image in them
-nonDockerDirs=(tests/ docs/)
-
-# remove directories we don't want to test
-for target in "${nonDockerDirs[@]}"; do
-  for i in "${!versions[@]}"; do
-    if [[ ${versions[i]} = "$target" ]]; then
-      unset 'versions[i]'
-    fi
-  done
-done
-echo "${versions[@]}"
-
-for dir in "${versions[@]}"; do
-  cd $dir
+versions=$(find . -maxdepth 1 -type d -name '[0-9]*' -exec basename {} \;)
+for dir in ${versions}; do
+  pushd $dir
   run_tests $dir
+  popd
 done
 
 exit
