b8360daf9023a3036cdd1f542d3b777625c26e86
Amanda Murphy <amurphy@newrelic.com>
2019-11-01 14:44:08 -0700

    PR comments

--- tools/update-docker-image.bash
+++ tools/update-docker-image.bash
@@ -7,11 +7,13 @@ if [[ ! "$1" =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]{3} ]]; then
 fi
 
 version=$1
+version_short=$(echo $1 | awk -F "." '{print $1 "." $2 "." $3}')
 
-IFS='.'
-read -ra ADDR <<< "$1"
-IFS=' '
-version_short="${ADDR[0]}.${ADDR[1]}.${ADDR[2]}"
+wget -O /tmp/newrelic-php5-${version}-linux-musl.tar.gz https://download.newrelic.com/php_agent/archive/${version}/newrelic-php5-${version}-linux-musl.tar.gz;
+
+sha=$(sha256sum /tmp/newrelic-php5-${version}-linux-musl.tar.gz | awk '{print $1}')
+
+echo "${sha}"
 
 mkdir $version_short
 
@@ -19,7 +21,8 @@ cp docker-entrypoint-template $version_short/docker-entrypoint.sh
 cd $version_short
 
 sed s/"ENV[[:space:]]NEWRELIC_VERSION/ENV NEWRELIC_VERSION ${version}"/ ../Dockerfile-template > Dockerfile
+sed -i s/"ENV[[:space:]]NEWRELIC_SHA/ENV NEWRELIC_SHA ${sha}"/ Dockerfile
 
 git checkout -b $version_short
 git add .
-git commit -m "version bump"
+git commit -m "version bump to ${version_short}"
