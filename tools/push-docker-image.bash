84e1dac3f64dad3ab1702799a82b996b60c84b24
A Murphy <amurphy@newrelic.com>
2019-11-04 14:35:05 -0800

    PHP-2279: Release automation (#6)
    * Release automation

---
+++ tools/push-docker-image.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+yes | docker system prune -a
+
+version=$1
+
+cd ${version} || exit 1
+
+docker build -t newrelic/php-daemon:latest -t newrelic/php-daemon:${version} .
+
+docker login --username=newrelicphp --password ${DOCKER_HUB_PASSWORD}
+
+docker push newrelic/php-daemon
---
+++ tools/update-docker-image.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -ex
+
+if [[ ! "$1" =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]{3} ]]; then
+  echo "ERROR: A full version string must be passed in"
+  exit 1
+fi
+
+version=$1
+version_short=$(echo $1 | awk -F "." '{print $1 "." $2 "." $3}')
+
+wget -O /tmp/newrelic-php5-${version}-linux-musl.tar.gz https://download.newrelic.com/php_agent/archive/${version}/newrelic-php5-${version}-linux-musl.tar.gz;
+
+sha=$(sha256sum /tmp/newrelic-php5-${version}-linux-musl.tar.gz | awk '{print $1}')
+
+mkdir $version_short
+
+cp docker-entrypoint-template $version_short/docker-entrypoint.sh
+cd $version_short
+
+sed \
+   -e s/"ENV[[:space:]]NEWRELIC_VERSION/ENV NEWRELIC_VERSION ${version}"/ \
+   -e s/"ENV[[:space:]]NEWRELIC_SHA/ENV NEWRELIC_SHA ${sha}"/ \
+    ../Dockerfile-template > Dockerfile
+
+git checkout -b $version_short
+git add .
+git commit -m "version bump to ${version_short}"
