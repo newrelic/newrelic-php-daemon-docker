8c29df4e4be7f9ae16ddec954f58879413a031f1
Amber Sistla <asistla@newrelic.com>
2022-03-08 13:21:21 -0700

    Update image-tests.sh

--- tests/image-tests.sh
+++ tests/image-tests.sh
@@ -29,7 +29,9 @@ run_tests() {
 
   name=version-check
   docker run -d --name $name c-daemon
-  docker_logs=$(docker logs $name | grep ${1//\/})
+  docker --version
+  docker logs $name
+  docker_logs="$(docker logs $name | grep ${1//\/})"
   echo "docker logs==== " $docker_logs
   verify_output $name "check the version" -n "$(docker logs $name | grep ${1//\/})"
 
