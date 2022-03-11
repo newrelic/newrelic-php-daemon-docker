d843351470c4ad21b4b145666e03dfdd9c5430f8
Amber Sistla <asistla@newrelic.com>
2022-03-11 07:12:23 -0700

    Create pr_runner.yml (#47)
    Creating initial basic GHA workflow that duplicates the travis CI logic.
    
    Added sleep so we GHA wouldn't try to pull logs before the docker finished building.

--- tests/image-tests.sh
+++ tests/image-tests.sh
@@ -29,27 +29,33 @@ run_tests() {
 
   name=version-check
   docker run -d --name $name c-daemon
+  sleep 10
   verify_output $name "check the version" -n "$(docker logs $name | grep ${1//\/})"
 
   name=debug-daemon
   docker run -d --name $name c-daemon --loglevel debug
+  sleep 10
   verify_output $name "set the loglevel to debug" -n "$(docker logs $name | grep Debug)"
 
   name=debug-daemon-with-cmd
   docker run -d --name $name c-daemon --loglevel debug
+  sleep 10
   verify_output $name "set the loglevel to debug using cmd override" -n "$(docker logs $name | grep Debug)"
 
   name=no-logs
   docker run -d --name $name c-daemon --logfile /var/log/newrelic/newrelic.log
+  sleep 10
   verify_output $name "set the logfile to /var/log/newrelic/newrelic.log" -n "$(docker exec $name sh -c 'exec cat /var/log/newrelic/newrelic.log')"
 
   name=no-logs-with-cmd
   docker run -d --name $name c-daemon /usr/bin/newrelic-daemon --logfile /var/log/newrelic/newrelic.log
+  sleep 10
   verify_output $name "set the logfile to /var/log/newrelic/newrelic.log using cmd override" -n "$(docker exec $name sh -c 'exec cat /var/log/newrelic/newrelic.log')"
 
   name=cfg-debug
   echo "loglevel=debug" > newrelic.cfg
   docker run -d --name $name -v $PWD/newrelic.cfg:/etc/newrelic/newrelic.cfg c-daemon --c /etc/newrelic/newrelic.cfg
+  sleep 10
   verify_output $name "set the loglevel to debug using cfg file" -n "$(docker logs $name | grep Debug)"
 
 }
